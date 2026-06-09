85e051c0cff7ef8658a0ec9cb4222fb0b924173f
talimirchuk <talii.mirr@gmail.com>
2026-06-09 11:54:41 -0300

    Hice todo(gracias Lu❤❤)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { preguntar } from './lib/consola.ts';
 import { cifrar, descifrar } from './lib/funcionesCriptográficas.ts';
 
-let palabra: string = ''; // Preguntar por la palabra a cifrar o descifrar
-let claveTexto: string = ''; // Preguntar por la clave de cifrado (número)
-let accion: string = ''; // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
+let palabra: string = preguntar("Ingresá una palabra:"); // Preguntar por la palabra a cifrar o descifrar
+let claveTexto: string = preguntar("Ingresá la clave:"); // Preguntar por la clave de cifrado (número)
+let accion: string = preguntar("¿Qué querés hacer? (cifrar/descifrar):"); // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
 
 let resultado: string = ''; // COMPLETAR
+
+if(accion==="cifrar"){
+    resultado = cifrar(palabra, Number(claveTexto))
+}
+
+else if(accion==="descifrar"){
+    resultado = descifrar(palabra, Number(claveTexto))
+}
+
 console.log(resultado);
